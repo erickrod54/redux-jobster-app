9675a531d46d09c3cf4ce5c4ec9cf84260fa7031
Erick Rodriguez <erickrod54@gmail.com>
2022-09-19 01:52:43 -0400

    Jobster app - version 1 - App js - Features: Importing and placing Landing from pages directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Landing from "./pages/Landing";
+
+/**Jobster app - version 1 - App js - Features:
+ * 
+ *    -->Importing and placing 'Landing' from
+ *      pages directory
+ * 
+ * Note: this is part of the first changes to
+ * Jobster app
+ */
 
 function App() {
   return (
-    <div className="App">
-      <h2>Jobster App</h2>
+    <div>
+      <Landing />
     </div>
   );
 }
