b717188b41cc4accca8c4d270c7422031ad708bf
Erick Rodriguez <erickrod54@gmail.com>
2022-09-20 02:03:16 -0400

    Jobster app - version 1 - Components > index js - Features: Importing and exporting first component Logo Component -this index will help to keep organized the components while the app scales-

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+/**Jobster app - version 1 - Components > index js - Features:
+ * 
+ *    --> Importing and exporting first component
+ *        'Logo' Component. 
+ * 
+ * Note: this index will simplified the way that i
+ * import make in it easy as i create new Components
+ */
+
+import Logo from "./Logo";
+
+export { Logo }
