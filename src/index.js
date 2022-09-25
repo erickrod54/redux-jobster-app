df4b8ef43e3cc8e067aaf1af42d890f9441c76ae
Erick Rodriguez <erickrod54@gmail.com>
2022-09-24 20:16:20 -0400

    Jobster app - version 5 - index js - Features: Importing and placing store and provider to set up redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { store } from './store';
+import { Provider } from 'react-redux';
+
+/**Jobster app - version 5 - index js - Features:
+ * 
+ *   --> Importing and placing 'store' and 'Provider'
+ *       in order to provide the state for the slice
+ * 
+ * 
+ * Note: Remember 'Provider' is from redux and the 'store={store}'
+ * as prop will provide the feature. 
+ */
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
