42674fd14f3399516cb48e99f11d7b1cb8f80744
Erick Rodriguez <erickrod54@gmail.com>
2022-09-24 20:09:28 -0400

    Jobster app - version 5 - App js - Features: Importing component from dashboard directory, Setting up nested route for dashboard

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Landing, Error, Dashboard, Register } from "./pages";
+import { Landing, Error, Register } from "./pages";
+
+import { AddJob, AllJobs, Profile, Stats, SharedLayout  } from './pages/dashboard'
 
 /**implementing notifications library */
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-/**Jobster app - version 3 - App js - Features:
+/**Jobster app - version 5 - App js - Features:
  * 
- *    --> Importing and placing 'ToastContainer' 
- *        to handle notifications
+ *    --> Importing components from dashboard 
+ *        directory.
  * 
- *    --> Customizing styles for 'ToastContainer'. 
+ *    --> Setting up nested route for 'dashboard'
  * 
- * Note: i place the 'ToastContainer' in a root
- * directory
- * 
- * By the toastify documentation the 'ToastContainer'
- * is targeted by the name 'Toastify__toast' and all
- * kind of styles can be applied.
+ * Note: to set the nested route for dashboard i use
+ * the 'SharedLayout' and 'Stats' as index ( i can use 
+ * also any other page component ).
  */
 
 function App() {
   return (
     <Router>
+      
       <Routes>
-        <Route path="/" element={<Dashboard />}/>
+        {/**here i set the nested route */}
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />}/>
+          <Route path="/all-jobs" element={<AllJobs />}/>
+          <Route path="/add-jobs" element={<AddJob />}/>
+          <Route path="/profile" element={<Profile />}/>
+        </Route>
+
         <Route path="/landing" element={<Landing />}/>
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error />}/>
