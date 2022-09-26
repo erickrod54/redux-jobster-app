dab046bdb010afb7a3bdd64964499c833d0f8c2c
Erick Rodriguez <erickrod54@gmail.com>
2022-09-26 10:28:59 -0400

    Jobster app - version 6 - dashboard > index js - Features: Importing and exporting all the Components related to dashboard

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,15 @@
+import AddJob from "./AddJob";
+import AllJobs from "./AllJobs";
+import Profile from "./Profile";
+import Stats from "./Stats";
+import SharedLayout from "./SharedLayout";
+
+/**Jobster app - version 6 - dashboard > index js - Features:
+ * 
+ *    --> Importing and exporting all the Components related to
+ *        dashboard.     
+ * 
+ * Note: this is the index for dashboard
+ */
+
+export { AddJob, AllJobs, Profile, Stats, SharedLayout }
