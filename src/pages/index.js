0614d7f425a54b6edae96f6c088a9429ffd9d36e
Erick Rodriguez <erickrod54@gmail.com>
2022-09-26 10:12:42 -0400

    Jobster app - version 5 - Pages > index js - Features: Removing Dashboard to implement dashboard directory

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+/**Jobster app - version 5 - Pages > index js - Features:
+ * 
+ *    --> Removing 'Dashboard' to implement dashboard 
+ *        directory. 
+ * 
+ *    --> Adding 'ProtectedRoute' to the index 
+ * 
+ * Note: this index will simplified the way that i
+ * import make in it easy as i create new Components
+ */
+
+import Register from "./Register";
+import Error from "./Error";
+import Landing from "./Landing";
+import ProtectedRoute from "./ProtectedRoute";
+
+export { Register, Error, Landing, ProtectedRoute }
