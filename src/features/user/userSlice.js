abda5920d6671bdeb9c375788c2e41939b4e517a
Erick Rodriguez <erickrod54@gmail.com>
2022-09-27 01:48:28 -0400

    Jobster app - version 6 - userSlice js - Features: Building isSidebarOpen and togle actions, Building updating feature for Profile page, Importing and returning registerUserThunk, loginUserthunk, updateUserThunk from userThunk js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,84 +1,64 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from 'react-toastify';
-import customFetch from "../../utils/axios";
-import { addUserToLocalStorage, getUserFromLocalStorage } from "../../utils/localStorage";
+import { addUserToLocalStorage,
+         getUserFromLocalStorage,
+         removeUserFromLocalStorage } from "../../utils/localStorage";
+import { registerUserThunk,
+         loginUserThunk,
+         updateUserThunk } from "./userThunk";
 
-
-/**Jobster app - version 5 - userSlice js - Features:
+/**Jobster app - version 6 - userSlice js - Features:
  * 
- *    --> Importing and placing customFetch hook to
- *        build the API request for login and register         
+ *    --> Building 'isSidebarOpen', and'toogle' 
+ *       actions 
  * 
- *    --> Setting up the 'extraReducers' to handle
- *        the notifications for register and login
- *        (the responses are server made - node js)
+ *    --> Building updating feature for Profile
+ *        page.
  * 
- *    --> Importing and placing 'getUserFromLocalStorage'
- *        as user prop in 'initialState'.
+ *    --> Importing and returning 'registerUserThunk',
+ *        'loginUserThunk', 'updateUserThunk' from 
+ *        'userThunk'. 
  * 
- *    --> Implementing 'addUserToLocalStorage' to add 
- *        the 'user' to the 'localStorage'
- * 
- * Note: this '/auth/testingRegister' url is already
- * set in the server ( made with node js)
- * 
- * Reference to HTTP methods ==> 'https://course-api.com/slides/'
- * > HTTP methods
- * 
- *  by this version i implement 'post', but 'get' and 'pos't use the
- * same end-point but i have to make sure to use the right method
+ * Note: These actions are going to brr used for 
+ * the 'SmallSidebar' and 'Navbar'
  */
 
 const initialState = { 
     isLoading: false,
-    /**the 'getUserFromLocalStorage' must be invoked*/
+    isSidebarOpen:false,
     user: getUserFromLocalStorage(),
 };
 
 export const registerUser = createAsyncThunk(
-    'user/registerUser', async(user,thunkAPI) =>
-{
-    /**here i set the try-cath to make the API request */
-    try {
-        /**the server needs end-point 'url' and 'user' with post method*/
-        
-        /**i switch the 'testingRegister' to 'register' and 
-         * effectivelly register the user*/
-        const resp = await customFetch.post('/auth/register', user )
-        console.log('this is the axios response when i send the user data ==>', resp)
-        return resp.data
-    } catch (error) {
-        /**using 'thunkAPI' to handle the error response from 
-         * axios */
-       return thunkAPI.rejectWithValue(error.response.data.msg);
-        
-    }
+    'user/registerUser', async(user,thunkAPI) => {
+     return registerUserThunk('/auth/register', user, thunkAPI)   
 })
 
-/**the logic for login user will be exactlly the same
- * that register */
 export const loginUser = createAsyncThunk(
-    'user/loginUser', async(user,thunkAPI) =>
-{
-    /**here i set the try-cath to make the API request */
-    try {
-        /**the server needs end-point 'url' and 'user' with post method*/
-        const resp = await customFetch.post('/auth/login', user )
-        console.log('this is the axios response when i send the login user data ==>', resp)
-        return resp.data
-    } catch (error) {
-        /**using 'thunkAPI' to handle the error response from 
-         * axios */
-       return thunkAPI.rejectWithValue(error.response.data.msg);
-        
-    }
+    'user/loginUser', async(user,thunkAPI) => {
+    return loginUserThunk('/auth/login', user, thunkAPI)
 })
+
+export const updateUser = createAsyncThunk(
+    'user/updateUser', async(user, thunkAPI) => {
+     return updateUserThunk('/auth/updateUser', user, thunkAPI)
+    }
+)
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
-    /**extraReducers will be 'pending', fulfilled, and 
-     * rejected */
+    /**here i build the actions - */
+    reducers:{
+        toggleSidebar:(state) => {
+            state.isSidebarOpen = !state.isSidebarOpen;
+        },
+        logoutUser: (state) => {
+            state.user = null
+            state.isSidebarOpen = false
+            removeUserFromLocalStorage()
+        },
+    },
     extraReducers:{
         [registerUser.pending]:(state) =>{
             state.isLoading = true;
@@ -120,8 +100,31 @@ const userSlice = createSlice({
             state.isLoading = false;
             toast.error(payload)
         },
+        /**here i have the extraReducers to
+         * update the value*/
+         [updateUser.pending]:(state) =>{
+            state.isLoading = true;
+        },
+        /**i destructure the payload that will be the user */
+        [updateUser.fulfilled]:(state, { payload }) =>{
+            const { user } = payload;
+            state.isLoading = false;
+            state.user = user;
+             /**here i add the user to local storage */
+             addUserToLocalStorage(user);
+            /**i set the toast with the user name */
+            toast.success(`User updated !!`)
+        },
+        /**i set the axios error message from toast*/
+        [updateUser.rejected]:(state, { payload }) =>{
+            state.isLoading = false;
+            toast.error(payload)
+        },
     }
 })
+
+
+export const { toggleSidebar, logoutUser } = userSlice.actions;
 
 /**here i export the reducer */
 export default userSlice.reducer;
