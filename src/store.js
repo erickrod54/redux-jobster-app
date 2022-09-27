583e29b6d262e126147f77f21028facc19e9f267
Erick Rodriguez <erickrod54@gmail.com>
2022-09-27 01:49:38 -0400

    Jobster app - version 6 - store js - Features: Building the store, Adding jobSlice to the store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from "./features/user/userSlice";
+import jobSlice from './features/job/jobSlice';
 
-/**Jobster app - version 5 - store js - Features:
+/**Jobster app - version 6 - store js - Features:
  * 
  *   --> Builting the 'store' 
  * 
+ *   --> Adding 'jobSlice' to the store.
  * 
  * Note: the store will reduce all state slices. 
  */
 
 export const store = configureStore({
     reducer:{
-        user: userSlice
+        user: userSlice,
+        job:jobSlice
     }
 })
