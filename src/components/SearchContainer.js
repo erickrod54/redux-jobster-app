5aca5717d950309fa38f25ec3912227a11e8f291
Erick Rodriguez <erickrod54@gmail.com>
2022-09-29 13:26:36 -0400

    Jobster app - version 6 - SearchContainer js - Features: Basic SearchContainer to display in AllJobs page

---
+++ src/components/SearchContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**Jobster app - version 6 - SearchContainer js - Features:
+ * 
+ *    --> Basic 'SearchContainer' to display 
+ * 
+ * Note: this component will display a spinner to implement
+ * for every component that needs time to fetch info
+ */
+
+const SearchContainer = () => {
+
+    return(
+        <h2>SearchContainer Component</h2>
+    )
+}
+
+export default SearchContainer;
