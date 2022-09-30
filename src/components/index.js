ade372004507863ce8fa0f3d3a9fbb62a2cfdac3
Erick Rodriguez <erickrod54@gmail.com>
2022-09-30 13:46:35 -0400

    Jobster app - version 7 - Components > index js  - Features: Importing and exporting SearchContainer, JobContainer and Job Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-/**Jobster app - version 6 - Components > index js - Features:
+/**Jobster app - version 7 - Components > index js - Features:
  * 
- *    --> Importing and exporting 'FormRowSelect' 
- *        Component. 
+ *    --> Importing and exporting 'SearchContainer',
+ *        'JobContainer', and 'Job' Component. 
  * 
  * Note: this component is created to applied the style
  * to every basic form row that i will build in the app
@@ -13,9 +13,16 @@ import Navbar from "./Navbar";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
 import FormRowSelect from "./FormRowSelect";
+import SearchContainer from "./SearchContainer";
+import Job from "./Job";
+import JobsContainer from "./JobsContainer";
 
 export { Logo, FormRow,
          Navbar,
          BigSidebar,
          SmallSidebar,
-         FormRowSelect }
+         FormRowSelect,
+        SearchContainer,
+        Job,
+        JobsContainer
+        }
