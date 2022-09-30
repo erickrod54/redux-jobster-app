015d17b1c3a3d8c922382211e4e5db957eac0a9f
Erick Rodriguez <erickrod54@gmail.com>
2022-09-30 13:49:22 -0400

    Jobster app - version 7 - Jobs js - Features: Building Jobs Component ( this component will render in allJobs )

---
+++ src/components/Job.js
@@ -0,0 +1,58 @@
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Job';
+import { useDispatch } from 'react-redux';
+import JobInfo from './JobInfo';
+
+/**Jobster app - version 7 - jobs js - Features:
+ * 
+ *    --> Building 'Job' Component.  
+ * 
+ * Note: this component will display the 
+ * information Card on 'allJobs' with all
+ * related to the job application.
+ */
+
+const Job = ({ id, position,
+             company, jobLocation, 
+             jobType, createdAt, status }) => {
+
+    const dispatch = useDispatch()
+
+    const date = createdAt;
+    return(
+        <Wrapper>
+        <header>
+            <div className='main-icon'>{company.charAt(0)}</div>
+            <div className='info'>
+                <h5>{position}</h5>
+                <p>{company}</p>
+            </div>
+        </header>
+        <div className='content'>
+            <div className='content-center'>
+                <JobInfo icon={<FaLocationArrow />} text={jobLocation}/>
+                <JobInfo icon={<FaCalendarAlt />} text={date}/>
+                <JobInfo icon={<FaBriefcase />} text={jobType}/>
+                <div className={`status ${status}`}>{status}</div>
+            </div>
+            <footer>
+                <div className='actions'>
+                    <Link to='/add-job'
+                         className='btn edit-btn'
+                         onClick={() => console.log('edit job')}>
+                        Edit{''}
+                    </Link>
+                    <button 
+                        className='btn delete-btn' 
+                        onClick={() => console.log('delete job')}>
+                        delete
+                    </button>
+                </div>
+            </footer>
+        </div>
+        </Wrapper>
+    )
+}
+
+export default Job;
