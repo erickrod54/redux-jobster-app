53cae2beee5c7ec36e00faa16b9a502254278413
Erick Rodriguez <erickrod54@gmail.com>
2022-10-05 00:34:55 -0400

    Jobster app - version 11 - allJobsSlice js - Features: Importing and placing getAllJobsThunk and showStats to simplify the code

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -1,33 +1,19 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
-import authHeader from '../../utils/AuthHeader';
-import customFetch from '../../utils/axios';
+import { getAllJobsThunk, showStatsThunk } from './allJobsThunk';
 
-/**Jobster app - version 10 - 'jobSlice' js - 
+/**Jobster app - version 11 - 'jobSlice' js - 
  * Features:
  * 
- *    --> Building 'changePage' action and exporting
- *        it.
+ *    --> Importing and placing 'getAllJobsThunk' request.
+ *        'getAllJobs' to simplify the code.     
  * 
- *    --> Building the 'Query String Params' for the 
- *        'url'
- * 
- *    --> Fixing 'handleChange' bug that preserves
- *        the old 'page' state after the search.
+ *    --> Importing and placing 'showStatsThunk' request
+ *        'showStats' to simplify the code.
  *  
- * Note: this request is the las request for
- * this file
+ * Note: Placing the thunks in a separate file is part of
+ * simplifying the code in this file 
  * 
- * Once 'getAllJobs' get fulfilled, i'll focus on these
- * two props 'totalJobs', and 'numOfPages' (they were created 
- * at initialState)
- * 
- * the params use to build the 'url' dynamic are 
- * 'initialFiltersState'
- * 
- * Fixing 'handleChange' (to troubleshoot and see this
- * error - Chrome > Network tab, and comment handleChange
- * 'state.page = 1' )
  */
 
 
@@ -53,45 +39,9 @@ const initialState = {
   ...initialFiltersState,
 };
 
-export const getAllJobs = createAsyncThunk('allJobs/getJobs', async(_,thunkAPI) => {
+export const getAllJobs = createAsyncThunk('allJobs/getJobs', getAllJobsThunk)
 
-  const { page, search, searchStatus, searchType, sort } = thunkAPI.getState().allJobs
-  
-  /**part of this configuration is server made to set
-   * the keys for example 'jobs?status',' &jobType=' 
-   * and so on..*/
-
-  /**now all the 'keys' are state and make the url 
-   * dynamic */
-  let url = 
-  `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`
-
-  /**only if search exist i append it to url */
-  if (search) {
-    url = url + `&search=${search}`
-  }
-  try {
-    const resp = await customFetch.get(url, authHeader(thunkAPI))
-    /**here i can test that i recieve the data in the
-     * front-end*/
-    console.log('all the jobs data ==>', resp.data)
-    return resp.data
-  } catch (error) {
-    return thunkAPI.rejectWithValue('There was an error')
-  }
-})
-
-export const showStats = createAsyncThunk('allJobs/showStats', async(_,thunkAPI) => {
-  try {
-    /**here i have to pass the header > user > token to get the stats and 
-    * avoid a 401 unauthorized access*/
-    const resp = await customFetch.get('/jobs/stats', authHeader(thunkAPI))
-    console.log('this is the ShowStats result ==>',resp.data);
-    return resp.data
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data.msg)
-  }
-})
+export const showStats = createAsyncThunk('allJobs/showStats', showStatsThunk)
 
 const allJobsSlice = createSlice({
     name: 'allJobs',
